5f18ca5bf618fcd6f636c2164341700af2bb70a6
arcogabbo <arcogabbo@gmail.com>
2022-07-28 18:38:38 +0200

    major refactor

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,10 @@
 import * as dotenv from "dotenv";
 dotenv.config();
 
+import * as TE from "fp-ts/lib/TaskEither";
+import * as t from "io-ts";
+import { wrapWithKind } from "@pagopa/io-functions-commons/dist/src/utils/types";
+import * as E from "fp-ts/lib/Either";
 import {
   CollectionMeta,
   DocumentClient as DocumentDBClient,
@@ -15,58 +19,71 @@ import {
   PROFILE_COLLECTION_NAME,
   ProfileModel,
   Profile,
-} from "io-functions-commons/dist/src/models/profile";
+} from "@pagopa/io-functions-commons/dist/src/models/profile";
 import {
   SERVICE_COLLECTION_NAME,
   ServiceModel,
   Service,
-} from "io-functions-commons/dist/src/models/service";
+  NewService,
+} from "@pagopa/io-functions-commons/dist/src/models/service";
 import {
   MESSAGE_COLLECTION_NAME,
   MessageModel,
   NewMessageWithoutContent,
-} from "io-functions-commons/dist/src/models/message";
+} from "@pagopa/io-functions-commons/dist/src/models/message";
 import {
   MESSAGE_STATUS_COLLECTION_NAME,
   MessageStatusModel,
   MessageStatus,
-} from "io-functions-commons/dist/src/models/message_status";
+  NewMessageStatus,
+} from "@pagopa/io-functions-commons/dist/src/models/message_status";
 import {
   NOTIFICATION_COLLECTION_NAME,
   NotificationModel,
   NewNotification,
-} from "io-functions-commons/dist/src/models/notification";
+} from "@pagopa/io-functions-commons/dist/src/models/notification";
 import {
   NOTIFICATION_STATUS_COLLECTION_NAME,
   NotificationStatusModel,
   NotificationStatus,
-} from "io-functions-commons/dist/src/models/notification_status";
-import * as documentDbUtils from "io-functions-commons/dist/src/utils/documentdb";
-import { getRequiredStringEnv } from "io-functions-commons/dist/src/utils/env";
-
-import { toString } from "fp-ts/lib/function";
+  NewNotificationStatus,
+} from "@pagopa/io-functions-commons/dist/src/models/notification_status";
+// import * as documentDbUtils from "@pagopa/io-functions-commons/dist/src/utils/documentdb";
+import { getRequiredStringEnv } from "@pagopa/io-functions-commons/dist/src/utils/env";
+// import { toString } from "fp-ts/lib/function";
+import * as PR from "io-ts/PathReporter";
 
 import * as ulid from "ulid";
 import * as faker from "faker";
 import * as randomstring from "randomstring";
 
-import { NonEmptyString } from "italia-ts-commons/lib/strings";
-import { FiscalCode } from "io-functions-commons/dist/generated/definitions/FiscalCode";
-import { MessageStatusValueEnum } from "io-functions-commons/dist/generated/definitions/MessageStatusValue";
-import { NotificationChannelStatusValueEnum } from "io-functions-commons/dist/generated/definitions/NotificationChannelStatusValue";
+import { IWithinRangeStringTag, NonEmptyString } from "@pagopa/ts-commons/lib/strings";
+import { FiscalCode } from "@pagopa/io-functions-commons/dist/generated/definitions/FiscalCode";
+import { MessageStatusValueEnum } from "@pagopa/io-functions-commons/dist/generated/definitions/MessageStatusValue";
+import { NotificationChannelStatusValueEnum } from "@pagopa/io-functions-commons/dist/generated/definitions/NotificationChannelStatusValue";
 
 import {
   createBlobService,
   createQueueService,
   createTableService,
 } from "azure-storage";
-import { MessageContent } from "io-functions-commons/dist/generated/definitions/MessageContent";
+import { MessageContent } from "@pagopa/io-functions-commons/dist/generated/definitions/MessageContent";
+import { CosmosClient } from "@azure/cosmos";
+import { pipe } from "fp-ts/lib/function";
+import { CosmosdbModelVersioned } from "@pagopa/io-functions-commons/dist/src/utils/cosmosdb_model_versioned";
+import { BaseModel } from "@pagopa/io-functions-commons/dist/src/utils/cosmosdb_model";
+import { NewMessageContent } from "@pagopa/io-functions-commons/dist/generated/definitions/NewMessageContent";
 
 const cosmosDbKey = getRequiredStringEnv("COSMOSDB_KEY");
 const cosmosDbUri = getRequiredStringEnv("COSMOSDB_URI");
 const cosmosDbName = getRequiredStringEnv("COSMOSDB_NAME");
 
-const documentDbDatabaseUrl = documentDbUtils.getDatabaseUri(cosmosDbName);
+const dbClient = new CosmosClient({
+  endpoint: cosmosDbUri,
+  key: cosmosDbKey
+})
+
+const dbInstance = dbClient.database(cosmosDbName);
 
 const documentClient = new DocumentDBClient(cosmosDbUri, {
   masterKey: cosmosDbKey,
@@ -135,14 +152,12 @@ function createCollection(
   });
 }
 
-const servicesCollectionUrl = documentDbUtils.getCollectionUri(
-  documentDbDatabaseUrl,
-  SERVICE_COLLECTION_NAME
-);
-const serviceModel = new ServiceModel(documentClient, servicesCollectionUrl);
 
-const getServiceFixture = (s?: Partial<Service>): Service =>
-  Service.decode({
+const serviceCollection = dbInstance.container(SERVICE_COLLECTION_NAME);
+const serviceModel = new ServiceModel(serviceCollection);
+
+const getServiceFixture = (s?: Partial<Service>): NewService =>
+  pipe(NewService.decode({
     authorizedCIDRs: [],
     authorizedRecipients: [],
     departmentName: faker.random.words(2),
@@ -157,18 +172,17 @@ const getServiceFixture = (s?: Partial<Service>): Service =>
     serviceId: ulid.ulid(),
     serviceName: faker.company.bsBuzz(),
     ...s,
-  } as Service).getOrElseL((err) => {
-    throw new Error("Cannot decode service payload:" + toString(err));
-  });
+  }),
+    E.getOrElseW((err) => {
+      throw new Error(`Cannot decode service payload: ${PR.failure(err).join("\n")}`)
+    })
+  )
 
-const profilesCollectionUrl = documentDbUtils.getCollectionUri(
-  documentDbDatabaseUrl,
-  PROFILE_COLLECTION_NAME
-);
-const profileModel = new ProfileModel(documentClient, profilesCollectionUrl);
+const profilesCollection = dbInstance.container(PROFILE_COLLECTION_NAME)
+const profileModel = new ProfileModel(profilesCollection);
 
 const getProfileFixture = (p?: Partial<Profile>): Profile =>
-  Profile.decode({
+  pipe(Profile.decode({
     acceptedTosVersion: faker.random.number(2),
     email: faker.internet.exampleEmail(),
     fiscalCode: generateFakeFiscalCode(),
@@ -180,17 +194,15 @@ const getProfileFixture = (p?: Partial<Profile>): Profile =>
       [ulid.ulid()]: [faker.random.arrayElement(["WEBHOOK", "EMAIL"])],
     },
     ...p,
-  } as Profile).getOrElseL((err) => {
-    throw new Error("Cannot decode profile payload:" + toString(err));
-  });
+  }),
+    E.getOrElseW(err => {
+      throw new Error(`Cannot decode profile payload: ${PR.failure(err).join("\n")}`)
+    })
+  )
 
-const messageCollectionUrl = documentDbUtils.getCollectionUri(
-  documentDbDatabaseUrl,
-  MESSAGE_COLLECTION_NAME
-);
+const messageCollection = dbInstance.container(MESSAGE_COLLECTION_NAME)
 const messageModel = new MessageModel(
-  documentClient,
-  messageCollectionUrl,
+  messageCollection,
   // tslint:disable-next-line: no-any
   "message-content" as any
 );
@@ -198,189 +210,230 @@ const messageModel = new MessageModel(
 const getMessageFixture = (
   m: Partial<NewMessageWithoutContent>
 ): NewMessageWithoutContent =>
-  NewMessageWithoutContent.decode({
-    id: ulid.ulid(),
-    indexedId: ulid.ulid(),
-    fiscalCode: generateFakeFiscalCode(),
-    createdAt: faker.date.past(),
-    senderServiceId: faker.random.word() as NewMessageWithoutContent["senderServiceId"],
-    senderUserId: faker.random.word() as NewMessageWithoutContent["senderUserId"],
-    isPending: faker.random.boolean(),
-    timeToLiveSeconds: faker.random.number({
-      min: 3600,
-      max: 10000,
-    }) as NewMessageWithoutContent["timeToLiveSeconds"],
-    ...m,
-  } as NewMessageWithoutContent).getOrElseL((err) => {
-    throw new Error("Cannot decode message payload:" + toString(err));
-  });
+  pipe(
+    NewMessageWithoutContent.decode({
+      id: ulid.ulid(),
+      indexedId: ulid.ulid(),
+      fiscalCode: generateFakeFiscalCode(),
+      createdAt: faker.date.past(),
+      senderServiceId: faker.random.word() as NewMessageWithoutContent["senderServiceId"],
+      senderUserId: faker.random.word() as NewMessageWithoutContent["senderUserId"],
+      isPending: faker.random.boolean(),
+      timeToLiveSeconds: faker.random.number({
+        min: 3600,
+        max: 10000,
+      }) as NewMessageWithoutContent["timeToLiveSeconds"],
+      ...m,
+    }),
+    E.getOrElseW(err => {
+      throw new Error(`Cannot decode profile payload: ${PR.failure(err).join("\n")}`)
+    })
+  )
 
-const messageStatusCollectionUrl = documentDbUtils.getCollectionUri(
-  documentDbDatabaseUrl,
-  MESSAGE_STATUS_COLLECTION_NAME
-);
+const messageStatusCollection = dbInstance.container(MESSAGE_STATUS_COLLECTION_NAME)
 const messageStatusModel = new MessageStatusModel(
-  documentClient,
-  messageStatusCollectionUrl
+  messageStatusCollection
 );
 
-const getMessageStatusFixture = (ms?: Partial<MessageStatus>): MessageStatus =>
-  MessageStatus.decode({
-    messageId: ulid.ulid(),
-    status: faker.random.arrayElement([
-      MessageStatusValueEnum.ACCEPTED,
-      MessageStatusValueEnum.FAILED,
-      MessageStatusValueEnum.PROCESSED,
-      MessageStatusValueEnum.REJECTED,
-      MessageStatusValueEnum.THROTTLED,
-    ]),
-    updatedAt: faker.date.past(),
-    ...ms,
-  } as MessageStatus).getOrElseL((err) => {
-    throw new Error("Cannot decode message status payload:" + toString(err));
-  });
+const getMessageStatusFixture = (ms?: Partial<MessageStatus>): NewMessageStatus =>
+  pipe(
+    NewMessageStatus.decode({
+      messageId: ulid.ulid(),
+      status: faker.random.arrayElement([
+        MessageStatusValueEnum.ACCEPTED,
+        MessageStatusValueEnum.FAILED,
+        MessageStatusValueEnum.PROCESSED,
+        MessageStatusValueEnum.REJECTED,
+        MessageStatusValueEnum.THROTTLED,
+      ]),
+      updatedAt: faker.date.past(),
+      ...ms,
+    }),
+    E.getOrElseW(err => {
+      throw new Error(`Cannot decode message status payload: ${PR.failure(err).join("\n")}`)
+    })
+  )
 
-const notificationCollectionUrl = documentDbUtils.getCollectionUri(
-  documentDbDatabaseUrl,
-  NOTIFICATION_COLLECTION_NAME
-);
+const notificationCollection = dbInstance.container(NOTIFICATION_COLLECTION_NAME)
 const notificationModel = new NotificationModel(
-  documentClient,
-  notificationCollectionUrl
+  notificationCollection
 );
 
 const getNotificationFixture = (
   n?: Partial<NewNotification>
 ): NewNotification =>
-  NewNotification.decode({
-    id: ulid.ulid(),
-    fiscalCode: generateFakeFiscalCode(),
-    messageId: ulid.ulid(),
-    channels: {
-      EMAIL: {
-        addressSource: "PROFILE_ADDRESS",
-        toAddress: faker.internet.exampleEmail(),
+  pipe(
+    NewNotification.decode({
+      id: ulid.ulid(),
+      fiscalCode: generateFakeFiscalCode(),
+      messageId: ulid.ulid(),
+      channels: {
+        EMAIL: {
+          addressSource: "PROFILE_ADDRESS",
+          toAddress: faker.internet.exampleEmail(),
+        },
+        WEBHOOK: {
+          url: "https://app-backend.io.italia.it/api/v1/notify?token=seceret",
+        },
       },
-      WEBHOOK: {
-        url: "https://app-backend.io.italia.it/api/v1/notify?token=seceret",
-      },
-    },
-    ...n,
-  } as NewNotification).getOrElseL((err) => {
-    throw new Error("Cannot decode notification payload:" + toString(err));
-  });
+      ...n,
+    }),
+    E.getOrElseW(err => {
+      throw new Error(`Cannot decode notification payload: ${PR.failure(err).join("\n")}`)
+    })
+  )
 
-const notificationStatusCollectionUrl = documentDbUtils.getCollectionUri(
-  documentDbDatabaseUrl,
-  NOTIFICATION_STATUS_COLLECTION_NAME
-);
+const notificationStatusCollection = dbInstance.container(NOTIFICATION_STATUS_COLLECTION_NAME)
 const notificationStatusModel = new NotificationStatusModel(
-  documentClient,
-  notificationStatusCollectionUrl
+  notificationStatusCollection
 );
 
 const getNotificationStatusFixture = (
   n?: Partial<NotificationStatus>
-): NotificationStatus =>
-  NotificationStatus.decode({
-    statusId: ulid.ulid(),
-    messageId: ulid.ulid(),
-    updatedAt: faker.date.past(),
-    status: faker.random.arrayElement([
-      NotificationChannelStatusValueEnum.EXPIRED,
-      NotificationChannelStatusValueEnum.FAILED,
-      NotificationChannelStatusValueEnum.THROTTLED,
-      NotificationChannelStatusValueEnum.SENT,
-    ]),
-    channel: faker.random.arrayElement(["EMAIL", "WEBHOOK"]),
-    ...n,
-  } as NotificationStatus).getOrElseL((err) => {
-    throw new Error(
-      "Cannot decode notification status payload:" + toString(err)
-    );
-  });
+): NewNotificationStatus =>
+  pipe(
+    NewNotificationStatus.decode({
+      statusId: ulid.ulid(),
+      messageId: ulid.ulid(),
+      updatedAt: faker.date.past(),
+      status: faker.random.arrayElement([
+        NotificationChannelStatusValueEnum.EXPIRED,
+        NotificationChannelStatusValueEnum.FAILED,
+        NotificationChannelStatusValueEnum.THROTTLED,
+        NotificationChannelStatusValueEnum.SENT,
+      ]),
+      channel: faker.random.arrayElement(["EMAIL", "WEBHOOK"]),
+      ...n,
+    }),
+    E.getOrElseW(err => {
+      throw new Error(`Cannot decode notification status payload: ${PR.failure(err).join("\n")}`)
+    })
+  )
 
 const generateServiceFixtures = async (s?: Partial<Service>) => {
   const aService = getServiceFixture(s);
-  const errorOrService = await serviceModel.create(
-    aService,
-    aService.serviceId
-  );
-  const createdService = errorOrService.getOrElseL(() => {
-    throw new Error("cannot create new service");
-  });
+
   // create a new version
-  await serviceModel.update(
-    createdService.id,
-    createdService.serviceId,
-    () => createdService
-  );
+  await pipe(
+    serviceModel.create(
+      aService
+    ),
+    TE.mapLeft(() => {
+      throw new Error("cannot create new service");
+    }),
+    TE.map(service => serviceModel.update(
+      service
+    ))
+  )()
 };
 
-const generateMessageContentFixture = () => {
-  return {
+const generateMessageContentFixture = (): NewMessageContent => {
+  return pipe(NewMessageContent.decode({
     markdown: faker.lorem.words(100),
-    subject: faker.lorem.words(5),
-  } as MessageContent;
+    subject: faker.lorem.words(5)
+  }),
+    E.getOrElseW(() => {
+      throw new Error("Cannot generate message content payload")
+    })
+  )
 };
 
 const generateMessageFixtures = async (fiscalCode: FiscalCode) => {
   const aMessage = getMessageFixture({
-    fiscalCode,
+    fiscalCode
   });
-  await messageModel.create(aMessage, aMessage.fiscalCode);
-  await messageModel.storeContentAsBlob(
-    blobService,
-    aMessage.id,
-    generateMessageContentFixture()
-  );
 
-  const aMessageStatus = getMessageStatusFixture({
-    messageId: aMessage.id,
-  });
-  const errorOrMessageStatus = await messageStatusModel.create(
-    aMessageStatus,
-    aMessageStatus.messageId
-  );
+  pipe(
+    messageModel.create(aMessage),
+    TE.chainW(_ => {
+      return messageModel.storeContentAsBlob(
+        blobService,
+        aMessage.id,
+        generateMessageContentFixture()
+      )
+    }),
+    TE.chainW(() => {
+      const aMessageStatus = getMessageStatusFixture({ messageId: aMessage.id });
 
-  const createdMessageStatus = errorOrMessageStatus.getOrElseL(() => {
-    throw new Error("cannot create new message status");
-  });
+      return messageStatusModel.create(
+        aMessageStatus
+      )
+    }),
+    //create a new version
+    TE.chainW(createdMessageStatus =>
+      messageStatusModel.update(
+        createdMessageStatus
+      )
+    ),
+    TE.bindW("aNotification", () => TE.of(getNotificationFixture({ messageId: aMessage.id, fiscalCode: aMessage.fiscalCode }))),
+    TE.chainFirstW(({ aNotification }) => notificationModel.create(aNotification)),
+    TE.bindW("aNotificationStatus", ({ aNotification }) => TE.of(getNotificationStatusFixture({ messageId: aMessage.id, notificationId: aNotification.id }))),
+    TE.bindW("createdNotificationStatus", ({ aNotificationStatus }) => notificationStatusModel.create(aNotificationStatus)),
+    TE.mapLeft((err) => {
+      throw new Error("Cannot create new notification status" + err);
+    }),
+    TE.chainW(({ createdNotificationStatus }) => notificationStatusModel.update(createdNotificationStatus)),
+  )
+
+  return aMessage.id
+
+  // ------------------------
+  // OLD CODE
+  // ------------------------
+
+  // await messageModel.create(aMessage, aMessage.fiscalCode);
+  // await messageModel.storeContentAsBlob(
+  //   blobService,
+  //   aMessage.id,
+  //   generateMessageContentFixture()
+  // );
+
+  // const aMessageStatus = getMessageStatusFixture({
+  //   messageId: aMessage.id,
+  // });
+  // const errorOrMessageStatus = await messageStatusModel.create(
+  //   aMessageStatus,
+  //   aMessageStatus.messageId
+  // );
+
+  // const createdMessageStatus = errorOrMessageStatus.getOrElseL(() => {
+  //   throw new Error("cannot create new message status");
+  // });
+  // // create a new version
+  // await messageStatusModel.update(
+  //   createdMessageStatus.id,
+  //   createdMessageStatus.messageId,
+  //   () => createdMessageStatus
+  // );
+
+  // const aNotification = getNotificationFixture({
+  //   messageId: aMessage.id,
+  //   fiscalCode: aMessage.fiscalCode,
+  // });
+  // await notificationModel.create(aNotification, aNotification.messageId);
+
+  // const aNotificationStatus = getNotificationStatusFixture({
+  //   messageId: aMessage.id,
+  //   notificationId: aNotification.id,
+  // });
+  // const errorOrNotifcationStatus = await notificationStatusModel.create(
+  //   aNotificationStatus,
+  //   aNotificationStatus.notificationId
+  // );
+
+  // const createdNotificationStatus = errorOrNotifcationStatus.getOrElseL(
+  //   (err) => {
+  //     throw new Error("cannot create new notification status:" + toString(err));
+  //   }
+  // );
   // create a new version
-  await messageStatusModel.update(
-    createdMessageStatus.id,
-    createdMessageStatus.messageId,
-    () => createdMessageStatus
-  );
+  // await notificationStatusModel.update(
+  //   createdNotificationStatus.id,
+  //   createdNotificationStatus.notificationId,
+  //   () => createdNotificationStatus
+  // );
 
-  const aNotification = getNotificationFixture({
-    messageId: aMessage.id,
-    fiscalCode: aMessage.fiscalCode,
-  });
-  await notificationModel.create(aNotification, aNotification.messageId);
-
-  const aNotificationStatus = getNotificationStatusFixture({
-    messageId: aMessage.id,
-    notificationId: aNotification.id,
-  });
-  const errorOrNotifcationStatus = await notificationStatusModel.create(
-    aNotificationStatus,
-    aNotificationStatus.notificationId
-  );
-
-  const createdNotificationStatus = errorOrNotifcationStatus.getOrElseL(
-    (err) => {
-      throw new Error("cannot create new notification status:" + toString(err));
-    }
-  );
-  // create a new version
-  await notificationStatusModel.update(
-    createdNotificationStatus.id,
-    createdNotificationStatus.notificationId,
-    () => createdNotificationStatus
-  );
-
-  return aMessage.id;
+  // return aMessage.id;
 };
 
 const generateUserFixtures = async () => {
@@ -442,6 +495,13 @@ createDatabase(cosmosDbName)
 
   .then(() =>
     NonEmptyString.decode(process.env.REQ_SERVICE_ID).fold(
+      () => generateServiceFixtures(),
+      (serviceId) => generateServiceFixtures({ serviceId })
+    )
+  )
+  //generate special service
+  .then(() =>
+    NonEmptyString.decode(process.env.REQ_SPECIAL_SERVICE_ID).fold(
       () => generateServiceFixtures(),
       (serviceId) => generateServiceFixtures({ serviceId })
     )
